b73e1bf667f775a53cd3d686e4658774adeed60c
Carlos Frederico Bastarz <cfbastarz@gmail.com>
2023-11-29 15:11:05 -0300

    Issue #9: reorganização do pacote, inclusão do script setup.py para realizar a instalação localmente ou a partir do PyPi

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='GSIBerror',
+    version='1.2.1a1',
+    url='https://gad-dimnt-cptec.github.io/GSIBerror',
+    author='Carlos Frederico Bastarz',
+    author_email='carlos.bastarz@inpe.br',
+    description='A Python class to read the records and attributes from the background error covariance matrices compatible with the Gridpoint Statistical Interpolation (in the .gcv file format).',
+    packages=find_packages(),
+    install_requires=[
+        ],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.8.2',
+)
